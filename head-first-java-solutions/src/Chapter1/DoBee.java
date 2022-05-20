e681606bff8d2504212ca817a53c744d6f33a168
kaushik.rao09 <kaushik.rao09@gmail.com>
2022-05-20 21:15:51 +0530

    2 exercise problems

---
+++ head-first-java-solutions/src/Chapter1/DoBee.java
@@ -0,0 +1,21 @@
+package Chapter1;
+
+/*
+ * This is the first exercise problem given in Page 13
+ */
+public class DoBee {
+
+	public static void main(String[] args) {
+		int x = 1;
+		while (x < 3) {
+			System.out.print("Doo");
+			System.out.print("Bee");
+			x = x + 1;
+		}
+		if (x == 3) {
+			System.out.print("Do");
+		}
+
+	}
+
+}
